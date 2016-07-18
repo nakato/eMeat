de9c922c9bce070b90a7499bdac27d62b4c83a91
Sachi King <nakato@nakato.io>
2016-07-18 18:57:22 +1000

    Move json middleware into middleware
    More middleware on the way, move json middleware into a dedicated
    directory.

--- emeat/main.py
+++ emeat/main.py
@@ -12,62 +12,12 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import json
 import logging
 
 import falcon
 
 from emeat.controllers import event
-
-
-class RequireJSON(object):
-
-    def process_request(self, req, resp):
-        if not req.client_accepts_json:
-            raise falcon.HTTPNotAcceptable(
-                'This API only supports responses encoded as JSON.',
-                href='http://json.org')
-
-        if req.method in ('POST', 'PUT'):
-            if 'application/json' not in req.content_type:
-                raise falcon.HTTPUnsupportedMediaType(
-                    'This API only supports requests encoded as JSON.',
-                    href='json.org')
-
-
-class JSONTranslator(object):
-
-    def process_request(self, req, resp):
-        # req.stream corresponds to the WSGI wsgi.input environ variable,
-        # and allows you to read bytes from the request body.
-        #
-        # See also: PEP 3333
-        if req.content_length in (None, 0):
-            if req.method in ('POST', 'PUT'):
-                raise falcon.HTTPBadRequest('A request body is required')
-            else:
-                return
-
-        body = req.stream.read()
-        if not body:
-            raise falcon.HTTPBadRequest('Empty request body',
-                                        'A valid JSON document is required.')
-
-        try:
-            req.context['doc'] = json.loads(body.decode('utf-8'))
-
-        except (ValueError, UnicodeDecodeError):
-            raise falcon.HTTPError(falcon.HTTP_753,
-                                   'Malformed JSON',
-                                   'Could not decode the request body. The '
-                                   'JSON was incorrect or not encoded as '
-                                   'UTF-8.')
-
-    def process_response(self, req, resp, resource):
-        if 'result' not in req.context:
-            return
-
-        resp.body = json.dumps(req.context['result'])
+from emeat.middleware import json
 
 
 def max_body(limit):
@@ -126,8 +76,8 @@ class eMeat_SetDescription(ControllerMixin):
 
 def main():
     application = falcon.API(middleware=[
-        RequireJSON(),
-        JSONTranslator(),
+        json.RequireJSON(),
+        json.JSONTranslator(),
     ])
 
     db_control = event.Event()
